cd28b8c0b4b6ed4cdef29a522200772044c9abc0
Parth Gupta <pgparth08@gmail.com>
2026-08-15 17:04:03 +0530

    feat: added some qutebrowser config

---
+++ tools/cli-tools/qutebrowser/config.py
@@ -0,0 +1,6 @@
+config.load_autoconfig()
+
+config.bind('<Ctrl+Shift+a>', 'set-cmd-text -s :tab-select')
+config.bind('<Ctrl+Shift+f>', 'set-cmd-text -s :tab-select')
+config.bind('<Ctrl+P>', 'set-cmd-text -s :tab-select')
+config.bind('gb', 'set-cmd-text -s :tab-select')
